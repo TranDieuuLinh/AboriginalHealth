9e0ec8eada5a9f2f268212f3a18bfce1fd4e07d1
TranDieuuLinh <dieulinh268268@gmail.com>
2024-08-26 22:11:38 +1000

    update signup form validation

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,38 +2,47 @@
     <div class="signUp-parent h-screen flex items-center justify-center">
       <div class="flex md:flex-row items-center justify-between bg-white sm:max-w-xl md:max-w-3xl lg:max-w-4xl rounded-2xl shadow-lg">
         
-        <!-- Form Section -->
         <div class="flex flex-col p-6 md:p-10 w-full md:w-1/2">
           <h1 class="text-3xl md:text-5xl  font-bold brownColorText mb-2">Join Us</h1>
           <p class="text-xs md:text-sm text-gray-400 mb-5">Help Us Build a Healthier Future for Aboriginal Communities</p>
-  
-          <div class="mb-4">
-            <label for="role" class="block text-xs md:text-sm font-medium text-gray-700">Role</label>
-            <select id="role" class="mt-1 block w-full md:w-[90%] p-2 border border-gray-300 text-gray-400 rounded-md shadow-sm appearance-none">
-              <option selected>I sign up as</option>
-              <option value="admin">an admin</option>
-              <option value="user">a general user</option>
-            </select>
-          </div>
-  
-          <div class="mb-4">
-            <label for="email" class="block text-xs md:text-sm font-medium text-gray-700">Email</label>
-            <input type="email" id="email" class="mt-1 block w-full md:w-[90%] text-xs md:text-base p-2 border border-gray-300 rounded-md shadow-sm" placeholder="Enter your email">
-          </div>
-  
-          <div class="mb-6">
-            <label for="password" class="block text-xs md:text-sm font-medium text-gray-700">Password</label>
-            <input type="password" id="password" class="mt-1 block w-full md:w-[90%] text-xs md:text-base p-2 border border-gray-300 rounded-md shadow-sm" placeholder="Enter your password">
-          </div>
-  
-          <button class="w-full md:w-[90%] text-xs md:text-base text-white py-2 px-4 rounded-md signUp-parent ">
-            Sign Up
-          </button>
-  
-          <p class="text-xs md:text-sm text-gray-600 mt-4">
-            Already have an account? 
-            <RouterLink to="/signin" class="text-blue-600 hover:text-blue-500">Sign in</RouterLink>
-          </p>
+          <form @submit.prevent="handleSubmission">
+            <div class="mb-4">
+                <label for="role" class="block text-xs md:text-sm font-medium text-gray-700">Role</label>
+                <select v-model="formData.role"  id="role" required
+                class="mt-1 block w-full md:w-[90%] p-2 border border-gray-300  rounded-md shadow-sm appearance-none"
+                :class="{'text-gray-400':formData.role === '', 'text-black':formData.role!==''}">
+                    <option value="" disabled selected>I sign up as...</option>
+                    <option value="admin">an admin</option>
+                    <option value="user">a general user</option>
+                </select>
+            </div>
+
+            <div class="mb-4">
+                <label for="phone" class="block text-xs md:text-sm font-medium text-gray-700">Phone</label>
+                <input type="tel" @blur="validatePhone(true)"  v-model="formData.phone" @input="validatePhone(false)" required id="phone" class="mt-1 block w-full md:w-[90%] text-xs md:text-base p-2 border border-gray-300 rounded-md shadow-sm" placeholder="Enter your phone">
+                <div v-if="errors.phone" class="text-orange-700 text-xs md:text-sm mt-1">{{ errors.phone }} </div>
+            </div>
+    
+            <div class="mb-4">
+                <label for="email" class="block text-xs md:text-sm font-medium text-gray-700">Email</label>
+                <input type="email" v-model="formData.email" required id="email" class="mt-1 block w-full md:w-[90%] text-xs md:text-base p-2 border border-gray-300 rounded-md shadow-sm" placeholder="Enter your email">
+            </div>
+    
+            <div class="mb-6">
+                <label for="password" class="block text-xs md:text-sm font-medium text-gray-700">Password</label>
+                <input type="password" @blur="validatePassword(true)" v-model="formData.password" @input="validatePassword" required id="password" class="mt-1 block w-full md:w-[90%] text-xs md:text-base p-2 border border-gray-300 rounded-md shadow-sm" placeholder="Enter your password">
+                <div v-if="errors.password" class="text-orange-700 text-xs md:text-sm mt-1">{{ errors.password }} </div>
+            </div>
+    
+            <button type="submit" class="w-full md:w-[90%] text-xs md:text-base text-white py-2 px-4 rounded-md signUp-parent ">
+                Sign Up
+            </button>
+    
+            <p class="text-xs md:text-sm text-gray-600 mt-4">
+                Already have an account? 
+                <RouterLink to="/signin" class="text-blue-600 hover:text-blue-500">Sign in</RouterLink>
+            </p>
+          </form>
         </div>
   
         <!-- Right Side: Image -->
@@ -46,7 +55,69 @@
   </template>
   
   <script setup>
-  // Your script here
+    import { ref, computed } from 'vue';
+
+    const validatePassword = (blur) => {
+    const password = formData.value.password;
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+
+    if (password.length < minLength) {
+        if (blur) errors.value.password = `Your password must be at least ${minLength} characters long.`;
+    } else if (!hasUpperCase) {
+        if (blur) errors.value.password = 'Your password must have at least 1 uppercase letter.';
+    } else if (!hasLowerCase) {
+        if (blur) errors.value.password = 'Your password must have at least 1 lowercase letter.';
+    } else {
+        errors.value.password = null; 
+    }
+    };
+
+
+    const validatePhone = (blur) => {
+        const phone = formData.value.phone
+        const digit = /\d/.test(phone)
+        const phoneCheck = /^0\d{9}$/.test(phone)
+
+        if (!phoneCheck){
+            if(blur) errors.value.phone = 'Your phone must starts with 0 and be exactly 10 digits';
+        } else if(!digit){
+            if (blur) errors.value.phone = 'Your phone must be a digit'
+        }   
+        else {
+            errors.value.phone = null;
+        }
+    }
+
+    const formData = ref({
+        password:'',
+        email:'',
+        phone:'',
+        role:''
+    });
+
+    const errors = ref({
+        password:null,
+        phone:null
+    });
+
+    const handleSubmission=() =>{
+        validatePassword(true),
+        validatePhone(true)
+        if( !errors.value.password && !errors.value.phone && formData.value.email!=='' && formData.value.password!=='' ){
+            clearForm();
+        }
+    };
+
+    const clearForm=() =>{
+        formData.value={
+            email: '',
+            password: '',
+            phone: '',
+            role:''
+        };
+    }
   </script>
   
   <style scoped>
